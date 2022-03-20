3acca88976b9dc85441e92ac97361d9542aec89b
ivanjo39191 <ivanjo39191@gmail.com>
2022-03-20 15:45:09 +0000

    fix: ajax get_product_retail_price

--- src/product/ajax.py
+++ src/product/ajax.py
@@ -8,6 +8,10 @@ def get_product_retail_price(request):
     retail_price = {}
     if request.POST:
         name = request.POST.get('name')
-        retail_price =  {'retail_price': Product.objects.get(name=name).retail_price}
-        return HttpResponse(json.dumps(retail_price), content_type='application/json')
+        if retail_price_obj := Product.objects.filter(name=name):
+            retail_price = retail_price_obj.first().retail_price
+        else:
+            retail_price = 0
+        retail_price_dict = {'retail_price': retail_price}
+        return HttpResponse(json.dumps(retail_price_dict), content_type='application/json')
     return HttpResponse(status=500)
